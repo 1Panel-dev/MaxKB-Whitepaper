007fd18c758c225fdda3f1e4f0af2b2d19f8f3ee
dove.zhu <dove.zhu@fit2cloud.com>
2025-10-27 11:47:56 +0800

    init

---
+++ docs/pages/cases/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+  sf: { title: 'JumpServer 护航顺丰科技超大规模资产安全运维' },
+  jxnx: { title: 'JumpServer 助力江苏农信行业云安全运维' },
+  xhs: { title: '小红书 JumpServer 大规模资产跨版本迁移之路' },
+  tencent: { title: '腾讯海外游戏基于JumpServer构建游戏安全运营能力' },
+  huolala: { title: '货拉拉通过JumpServer纳管大规模云上资产' },
+  wanhua: { title: '万华化学通过JumpServer管理全球化分布式IT资产，并且实现与云管平台的联动' }
+}
