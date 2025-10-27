f40aaf77a581b0b658ec14752218fec64b913ab2
xueqidove <60059766+xueqidove@users.noreply.github.com>
2025-10-27 18:57:08 +0800

    Update _meta.ts

--- docs/pages/_meta.ts
+++ docs/pages/_meta.ts
@@ -1,11 +1,16 @@
 export default {
-  index: { title: '1. 引言' },
-  history: { title: '2. 堡垒机发展历史回顾' },
-  newgen: { title: '3. 新一代堡垒机的内涵' },
-  architecture: { title: '4. 基于 JumpServer 构建新一代堡垒机' },
-  v3: { title: '5. JumpServer V3 版本介绍' },
-  v4: { title: '6. JumpServer V4 版本介绍' },
-  pam: { title: '7. JumpServer PAM 特权账号管理' },
-  cases: { title: '8. 案例研究&社区分享' },
-  contact: {title: '9. 联系我们' }
+  Introduction: { title: '1. 引言' },
+  Development: { title: '2.生成式 AI 的发展浪潮与智能体的兴起' },
+  Value: { title: '3.企业智能体的价值与建设挑战' },
+  Implementation: { title: '4.企业智能体的落地公式' },
+  Scenario: { title: '5.企业智能体的场景选择' },
+  Role: { title: '6.企业智能体的角色定位' },
+  Approach: { title: '7.企业智能体的实现范式' },
+  Path: { title: '8.企业智能体的落地路径' },
+  Construction: { title: '9.企业智能体的建设内容与组织支持' },
+  Technologies: { title: '10.企业智能体建设的关键技术' }, 
+  MaxKB: { title: '11.MaxKB：企业级智能体平台' }, 
+  Case: { title: '12.企业智能体建设成功案例' }, 
+  Reference: { title: '13.参考链接' }, 
+  contact: {title: '14. 联系我们' }
 }
